d8c2eaed28fab98802c044c1c611bab8ba04316b
Mikkel Thygesen <dkmithy@coloplast.com>
2022-11-25 01:08:17 +0100

    Added expandable, search and find

--- app/src/main/java/com/mikkelthygesen/billsplit/ui/features/shared_budget/SharedBudgetView.kt
+++ app/src/main/java/com/mikkelthygesen/billsplit/ui/features/shared_budget/SharedBudgetView.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -30,6 +31,8 @@ import com.mikkelthygesen.billsplit.models.Payment
 import com.mikkelthygesen.billsplit.models.Person
 import com.mikkelthygesen.billsplit.sampleSharedExpenses
 import com.mikkelthygesen.billsplit.tryCatchDefault
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun SharedBudgetView(
@@ -37,12 +40,22 @@ fun SharedBudgetView(
 ) {
     val flow = viewModel.shareableStateFlow().collectAsState(initial = emptyList())
     val shareablesState = flow.value.sortedBy { it.timeStamp }.reversed()
+    val lazyListState = LazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    var focusListItemIndex by remember {
+        mutableStateOf(-1)
+    }
+    LaunchedEffect(Unit) {
+        delay(5000L)
+        focusListItemIndex = -1
+    }
     LazyColumn(
         modifier = Modifier
             .padding(horizontal = 12.dp)
             .fillMaxHeight()
             .fillMaxWidth(),
-        reverseLayout = true
+        reverseLayout = true,
+        state = lazyListState
     ) {
         item {
             Row(Modifier.fillMaxWidth(), Arrangement.End) {
@@ -53,13 +66,13 @@ fun SharedBudgetView(
             count = shareablesState.size,
             key = { shareablesState[it].timeStamp }) { index ->
             Row(
-                Modifier.padding(vertical = 4.dp)
+                Modifier.padding(vertical = 4.dp),
+                Arrangement.SpaceEvenly
             ) {
                 val shareable = shareablesState[index]
-                val latestIndex = tryCatchDefault(true) {
-                    shareablesState[index - 1].createdBy != shareable.createdBy
-                }
-                if (shareable.createdBy != viewModel.getLoggedIn()) {
+                val latestIndex =
+                    tryCatchDefault(true) { shareablesState[index - 1].createdBy != shareable.createdBy }
+                if (shareable.createdBy != viewModel.getLoggedIn() && shareable !is Payment) {
                     if (latestIndex)
                         ProfilePicture(
                             person = shareable.createdBy,
@@ -71,22 +84,32 @@ fun SharedBudgetView(
                 Box(
                     modifier = Modifier
                         .weight(6f)
-                        .fillMaxWidth()
+                        .fillMaxWidth(),
                 ) {
                     val isLatestMessage = index == 0
                     when (shareable) {
                         is GroupExpense -> SharedExpenseListItem(
                             groupExpense = shareable,
+                            isFocused = index == focusListItemIndex,
                             isLastMessage = isLatestMessage
                         )
                         is Payment -> PaymentListView(payment = shareable)
                         is GroupExpensesChanged -> ChangesListView(
                             groupExpensesChanged = shareable,
-                            isLastMessage = isLatestMessage
+                            isLastMessage = isLatestMessage,
+                            onClickGoToExpense = { id ->
+                                coroutineScope.launch {
+                                    val groupExpense =
+                                        shareablesState.first { it is GroupExpense && it.id == id }
+                                    val indexOf = shareablesState.indexOf(groupExpense)
+                                    lazyListState.animateScrollToItem(indexOf)
+                                    focusListItemIndex = indexOf
+                                }
+                            }
                         )
                     }
                 }
-                if (shareable.createdBy == viewModel.getLoggedIn()) {
+                if (shareable.createdBy == viewModel.getLoggedIn() && shareable !is Payment) {
                     if (latestIndex)
                         ProfilePicture(
                             person = shareable.createdBy,
@@ -103,14 +126,14 @@ fun SharedBudgetView(
 
 @Composable
 private fun ProfilePicture(person: Person, modifier: Modifier) {
-        Image(
-            modifier = modifier
-                .aspectRatio(1f)
-                .clip(CircleShape),
-            painter = painterResource(id = person.pfpResId),
-            contentDescription = "Person profile picture for ${person.nameState}",
-            contentScale = ContentScale.Crop
-        )
+    Image(
+        modifier = modifier
+            .aspectRatio(1f)
+            .clip(CircleShape),
+        painter = painterResource(id = person.pfpResId),
+        contentDescription = "Person profile picture for ${person.nameState}",
+        contentScale = ContentScale.Crop
+    )
 }
 
 @Composable
@@ -118,7 +141,8 @@ private fun PaymentListView(payment: Payment) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 12.dp, horizontal = 8.dp)
+            .padding(vertical = 12.dp, horizontal = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "${payment.createdBy.nameState} paid $${payment.amount} to ${payment.paidTo.nameState}",
@@ -130,29 +154,27 @@ private fun PaymentListView(payment: Payment) {
 @Composable
 private fun ChangesListView(
     groupExpensesChanged: GroupExpensesChanged,
-    isLastMessage: Boolean
+    isLastMessage: Boolean,
+    onClickGoToExpense: (String) -> Unit
 ) {
     val original = groupExpensesChanged.groupExpenseOriginal
     val updated = groupExpensesChanged.groupExpenseEdited
     var expanded by remember {
         mutableStateOf(isLastMessage)
     }
-    Column(
-        modifier = Modifier
-            .clickable { expanded = !expanded }
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(10.dp))
-            .background(MaterialTheme.colors.primary)
-            .padding(vertical = 12.dp, horizontal = 8.dp)
-            .animateContentSize()
-
+    ExpandableView(
+        modifier = Modifier.clickable { expanded = !expanded },
+        expanded = expanded,
+        iconResId = R.drawable.ic_baseline_search_24,
+        onIconClick = { onClickGoToExpense(groupExpensesChanged.groupExpenseOriginal.id) }
     ) {
         Text(text = "${groupExpensesChanged.createdBy.nameState} made changes to an expense")
         if (expanded) {
             Row {
                 val wasTotalChanged = original.total != updated.total
                 val wasPayerChanged = original.payeeState != updated.payeeState
-                val wasDescriptionChanged = original.descriptionState != updated.descriptionState
+                val wasDescriptionChanged =
+                    original.descriptionState != updated.descriptionState
                 val wasParticipantsChanged =
                     original.getParticipants().size != updated.getParticipants().size
                 Column(Modifier.height(intrinsicSize = IntrinsicSize.Max), Arrangement.Top) {
@@ -197,85 +219,68 @@ private fun ChangesListView(
 private fun SharedExpenseListItem(
     viewModel: SharedBudgetViewModel = viewModel(),
     groupExpense: GroupExpense,
+    isFocused: Boolean,
     isLastMessage: Boolean
 ) {
     var expanded by remember {
         mutableStateOf(isLastMessage)
     }
-    Row(
-        modifier = Modifier
-            .animateContentSize()
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(10.dp))
-            .clickable { expanded = !expanded }
-            .background(MaterialTheme.colors.primary)
-            .padding(vertical = 12.dp, horizontal = 8.dp),
-        Arrangement.SpaceBetween
+    ExpandableView(
+        modifier = Modifier.let {
+            if(isFocused)
+                it.clip(RoundedCornerShape(10.dp))
+                    .background(MaterialTheme.colors.secondary)
+                    .padding(2.dp)
+                    .clickable { expanded = !expanded }
+            else it.clickable { expanded = !expanded }
+        },
+        expanded = expanded,
+        iconResId = R.drawable.ic_baseline_edit_24,
+        onIconClick = { viewModel.editSharedExpense(groupExpense) }
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .padding(end = 16.dp),
-            horizontalAlignment = if (!expanded) Alignment.CenterHorizontally else Alignment.Start
-        ) {
-            if (groupExpense.descriptionState.isNotBlank())
-                Text(
-                    text = "\"${groupExpense.descriptionState}\"",
-                    style = TextStyle(fontSize = 18.sp, fontStyle = FontStyle.Italic)
-                )
-            else Text(text = "${groupExpense.createdBy.nameState} added a new expense!")
+        if (groupExpense.descriptionState.isNotBlank())
+            Text(
+                text = "\"${groupExpense.descriptionState}\"",
+                style = TextStyle(fontSize = 18.sp, fontStyle = FontStyle.Italic)
+            )
+        else Text(text = "${groupExpense.createdBy.nameState} added a new expense!")
+        Box(modifier = Modifier.height(8.dp))
+        if (!expanded)
+            Text(
+                text = "$${groupExpense.total}",
+                style = TextStyle(fontStyle = FontStyle.Italic, fontSize = 20.sp)
+            )
+        else {
+            Text(text = "$${groupExpense.total} was paid by ${groupExpense.payeeState.nameState}")
             Box(modifier = Modifier.height(8.dp))
-            if (!expanded)
-                Text(
-                    text = "$${groupExpense.total}",
-                    style = TextStyle(fontStyle = FontStyle.Italic, fontSize = 20.sp)
-                )
-            else {
-                Text(text = "$${groupExpense.total} was paid by ${groupExpense.payeeState.nameState}")
-                Box(modifier = Modifier.height(8.dp))
-                Row {
-                    Column {
-                        if (groupExpense.sharedExpenseState > 0F)
-                            Text(text = "Shared")
-                        groupExpense.individualExpenses.map {
-                            if (it.expenseState > 0F) {
-                                Text(text = it.person.nameState)
-                            }
+            Row {
+                Column {
+                    if (groupExpense.sharedExpenseState > 0F)
+                        Text(text = "Shared")
+                    groupExpense.individualExpenses.map {
+                        if (it.expenseState > 0F) {
+                            Text(text = it.person.nameState)
                         }
                     }
-                    Column(modifier = Modifier.padding(horizontal = 8.dp)) {
-                        if (groupExpense.sharedExpenseState > 0f)
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Text(text = "$${groupExpense.sharedExpenseState} / ")
-                                val participants =
-                                    groupExpense.individualExpenses
-                                        .filter { it.isParticipantState }
-                                        .map { it.person }
-                                ParticipantsView(list = participants)
-                            }
-                        groupExpense.individualExpenses.map {
-                            if (it.expenseState > 0F) {
-                                Text(text = "$${it.expenseState}")
-                            }
+                }
+                Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    if (groupExpense.sharedExpenseState > 0f)
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text(text = "$${groupExpense.sharedExpenseState} / ")
+                            val participants =
+                                groupExpense.individualExpenses
+                                    .filter { it.isParticipantState }
+                                    .map { it.person }
+                            ParticipantsView(list = participants)
+                        }
+                    groupExpense.individualExpenses.map {
+                        if (it.expenseState > 0F) {
+                            Text(text = "$${it.expenseState}")
                         }
                     }
                 }
             }
         }
-        if (expanded)
-            IconButton(
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .fillMaxHeight(),
-                onClick = { viewModel.editSharedExpense(groupExpense) },
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_baseline_edit_24),
-                    contentDescription = "Edit expense",
-                    tint = MaterialTheme.colors.secondary
-                )
-            }
     }
 }
 
@@ -318,6 +323,48 @@ private fun ParticipantsView(list: List<Person>) {
     }
 }
 
+@Composable
+private fun ExpandableView(
+    modifier: Modifier = Modifier,
+    expanded: Boolean = false,
+    onIconClick: () -> Unit,
+    iconResId: Int,
+    content: @Composable () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .animateContentSize()
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(MaterialTheme.colors.primary)
+            .padding(vertical = 12.dp, horizontal = 8.dp),
+        Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(end = 16.dp),
+            horizontalAlignment = if (!expanded) Alignment.CenterHorizontally else Alignment.Start
+        ) {
+            content()
+        }
+        if (expanded)
+            IconButton(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .fillMaxHeight(),
+                onClick = onIconClick,
+            ) {
+                Icon(
+                    painter = painterResource(id = iconResId),
+                    contentDescription = "Edit expense",
+                    tint = MaterialTheme.colors.secondary
+                )
+            }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun PreviewMenu() {
@@ -328,7 +375,16 @@ private fun PreviewMenu() {
 @Composable
 private fun PreviewSharedExpenseListItem() {
     val groupExpense = sampleSharedExpenses.first()
-    Box(modifier = Modifier.height(200.dp)) {
-        SharedExpenseListItem(groupExpense = groupExpense, isLastMessage = true)
+    Box(
+        modifier = Modifier
+            .height(200.dp)
+            .padding(20.dp)
+    ) {
+        SharedExpenseListItem(
+            viewModel = SharedBudgetViewModel(),
+            groupExpense = groupExpense,
+            isLastMessage = true,
+            isFocused = true
+        )
     }
 }
