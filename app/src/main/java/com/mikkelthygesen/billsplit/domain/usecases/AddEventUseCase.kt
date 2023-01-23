6e47e1bc32be29a2be835c2318a28f1194ab51d6
Mikkel Thygesen <>
2023-01-23 21:34:12 +0100

    Saved event in db

--- app/src/main/java/com/mikkelthygesen/billsplit/domain/usecases/AddEventUseCase.kt
+++ app/src/main/java/com/mikkelthygesen/billsplit/domain/usecases/AddEventUseCase.kt
@@ -1,8 +1,11 @@
 package com.mikkelthygesen.billsplit.domain.usecases
 
 import com.mikkelthygesen.billsplit.data.local.database.BillSplitDb
+import com.mikkelthygesen.billsplit.data.local.database.model.GroupExpenseDb
 import com.mikkelthygesen.billsplit.data.remote.ServerApiImpl
+import com.mikkelthygesen.billsplit.data.remote.dto.EventDTO
 import com.mikkelthygesen.billsplit.domain.models.Group
+import com.mikkelthygesen.billsplit.domain.models.GroupExpense
 import com.mikkelthygesen.billsplit.domain.models.interfaces.Event
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
@@ -16,6 +19,11 @@ class AddEventUseCase @Inject constructor(
     suspend fun execute(group: Group, event: Event): Event {
         val dto = serverApiImpl.addEvent(group, event)
         database.groupsDao().insert(group.toDb())
+        when (dto) {
+            is EventDTO.ExpenseDTO -> database.eventsDao().insertGroupExpense(dto.toDb(group.id))
+            is EventDTO.ChangeDTO -> database.eventsDao().insertExpenseChange(dto.toDb(group.id))
+            is EventDTO.PaymentDTO -> database.eventsDao().insertPayment(dto.toDb(group.id))
+        }
         return dto.toEvent()
     }
 }
