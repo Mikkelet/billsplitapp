c328687f17d4cf62c89e365adc6ada2c8e181e97
Mikkel Thygesen <>
2023-01-28 13:47:41 +0100

    fixed dep error

--- app/src/main/java/com/mikkelthygesen/billsplit/data/local/database/daos/FriendsDao.kt
+++ app/src/main/java/com/mikkelthygesen/billsplit/data/local/database/daos/FriendsDao.kt
@@ -2,17 +2,17 @@ package com.mikkelthygesen.billsplit.data.local.database.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.mikkelthygesen.billsplit.data.local.database.model.FriendDb
 
 @Dao
 interface FriendsDao {
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(vararg friendDb: FriendDb)
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(friendDb: List<FriendDb>)
 
     @Query("SELECT * FROM friends")
