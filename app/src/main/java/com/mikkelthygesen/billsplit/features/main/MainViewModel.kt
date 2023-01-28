bd1b3a89eb77088a1ef9aa00a18ee7dfd2742255
Mikkel Thygesen <>
2023-01-28 20:49:38 +0100

    Fixed UploadProfilePicture button not loading

--- app/src/main/java/com/mikkelthygesen/billsplit/features/main/MainViewModel.kt
+++ app/src/main/java/com/mikkelthygesen/billsplit/features/main/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.mikkelthygesen.billsplit.features.main
 
-import android.net.Uri
-import com.mikkelthygesen.billsplit.domain.usecases.UploadProfilePictureUseCase
 import com.mikkelthygesen.billsplit.features.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,22 +7,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val uploadProfilePictureUseCase: UploadProfilePictureUseCase,
 ) : BaseViewModel() {
 
     object AddGroup : UiState
     object MyGroups : UiState
-
 
     override val _mutableUiStateFlow: MutableStateFlow<UiState> = MutableStateFlow(MyGroups)
 
     fun showMyGroups() = updateUiState(MyGroups)
 
     fun showAddGroup() = updateUiState(AddGroup)
-
-    suspend fun uploadProfilePhoto(uri: Uri, onSuccess: () -> Unit) {
-        val result = runCatching { uploadProfilePictureUseCase.execute(uri) }
-        result.foldSuccess { onSuccess() }
-    }
-
 }
--- app/src/main/java/com/mikkelthygesen/billsplit/features/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/mikkelthygesen/billsplit/features/main/profile/ProfileViewModel.kt
@@ -1,6 +1,8 @@
 package com.mikkelthygesen.billsplit.features.main.profile
 
+import android.net.Uri
 import com.mikkelthygesen.billsplit.domain.usecases.UpdateNameUseCase
+import com.mikkelthygesen.billsplit.domain.usecases.UploadProfilePictureUseCase
 import com.mikkelthygesen.billsplit.features.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -8,16 +10,24 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val updateNameUseCase: UpdateNameUseCase
-) : BaseViewModel() {
+    private val updateNameUseCase: UpdateNameUseCase,
+    private val uploadProfilePictureUseCase: UploadProfilePictureUseCase,
+
+    ) : BaseViewModel() {
 
     object Profile : UiState
     object FriendsPressed : UiEvent
+    object ProfilePictureUploaded : UiEvent
 
     override val _mutableUiStateFlow: MutableStateFlow<UiState> = MutableStateFlow(Profile)
 
     fun showFriends() {
         emitUiEvent(FriendsPressed)
+    }
+
+    suspend fun uploadProfilePhoto(uri: Uri) {
+        val result = runCatching { uploadProfilePictureUseCase.execute(uri) }
+        result.foldSuccess { emitUiEvent(ProfilePictureUploaded) }
     }
 
     fun signOut() {
