5896abca54e58765c06e42a96bd708eba44a4e89
Mikkel Thygesen <>
2023-01-28 22:24:59 +0100

    applied signout usecase

--- app/src/main/java/com/mikkelthygesen/billsplit/features/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/mikkelthygesen/billsplit/features/main/profile/ProfileViewModel.kt
@@ -1,18 +1,21 @@
 package com.mikkelthygesen.billsplit.features.main.profile
 
 import android.net.Uri
+import androidx.lifecycle.viewModelScope
+import com.mikkelthygesen.billsplit.domain.usecases.SignOutUseCase
 import com.mikkelthygesen.billsplit.domain.usecases.UpdateNameUseCase
 import com.mikkelthygesen.billsplit.domain.usecases.UploadProfilePictureUseCase
 import com.mikkelthygesen.billsplit.features.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
     private val updateNameUseCase: UpdateNameUseCase,
     private val uploadProfilePictureUseCase: UploadProfilePictureUseCase,
-
+    private val signOutUseCase: SignOutUseCase
     ) : BaseViewModel() {
 
     object Profile : UiState
@@ -31,6 +34,9 @@ class ProfileViewModel @Inject constructor(
     }
 
     fun signOut() {
-        authProvider.signOut()
+        viewModelScope.launch {
+            val response = runCatching { signOutUseCase.execute() }
+            response.foldSuccess {  }
+        }
     }
 }
